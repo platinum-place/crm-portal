ba1f22e4d28603dc22fb9b7d78b70c3d6343c21a
warlyn <warlyn200@gmail.com>
2025-03-17 12:00:09 -0400

    Add LoginController with basic routes and view setup
    Introduced a new LoginController with an index method to serve the login view. Added a corresponding Blade template and a route to access the login page. The login and logout methods are stubbed for future implementation.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function logout(Request $request)
+    {
+    }
+}
