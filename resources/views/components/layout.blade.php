7ab06d019e56cc11266f1f724f0e87bb87773f96
warlyn <warlyn200@gmail.com>
2025-03-18 08:29:12 -0400

    Add spacing adjustments in layout.blade.php
    Ensure proper spacing for readability and maintain code consistency. This change does not affect functionality but improves visual clarity in the layout file.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+
     @livewireStyles
 </head>
 
@@ -29,7 +30,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
-
 @livewireScripts
 </body>
 
